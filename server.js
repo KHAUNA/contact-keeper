6cc3a7265556f3c2c9b9d96e5e374bfc72ac45cf
FORREST <jtwinturbo@gmail.com>
2020-08-31 16:43:30 -0400

    express server setup with initial routes

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db')
+
 const app = express();
+
+//Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API' }))
 
 //Define routes
@@ -9,3 +15,4 @@ app.use('/api/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
